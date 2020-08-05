810b11402cae467dd2fc1d8ddb8fd98fee0f8657
Jason Allen <jason.allen@claytonhomes.com>
2020-08-05 18:45:56 -0400

    Add missing DB translation code

--- CMH.CS.ERP.IntegrationHub.Interpol.Configuration/InterpolScheduleDbModel.cs
+++ CMH.CS.ERP.IntegrationHub.Interpol.Configuration/InterpolScheduleDbModel.cs
@@ -5,8 +5,13 @@ namespace CMH.CS.ERP.IntegrationHub.Interpol.Configuration
     public class InterpolScheduleDbModel : InterpolDbModelWithBusinessUnits
     {
         public Guid InstanceKey { get; set; }
+
         public int PollingIntervalMilliseconds { get; set; }
+
         public string DaysOfWeek{ get; set; }
+
         public string DataTypes { get; set; }
+
+        public int MaximumReportInterval { get; set; }
     }
 }
