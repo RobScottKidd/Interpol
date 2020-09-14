9a9ad202edd499ee8761e04845ad86e118e4d006
Jason Allen <jason.allen@claytonhomes.com>
2020-09-13 21:54:06 -0400

    Remove unused interfaces and fields

--- CMH.CS.ERP.IntegrationHub.Interpol.Models/BusinessUnit.cs
+++ CMH.CS.ERP.IntegrationHub.Interpol.Models/BusinessUnit.cs
@@ -41,10 +41,5 @@ namespace CMH.CS.ERP.IntegrationHub.Interpol.Models
         /// Full BU name
         /// </summary>
         public string BUName { get; set; }
-
-        /// <summary>
-        /// Full Alternate name
-        /// </summary>
-        public string BUAlternateName { get; set; }
     }
 }
