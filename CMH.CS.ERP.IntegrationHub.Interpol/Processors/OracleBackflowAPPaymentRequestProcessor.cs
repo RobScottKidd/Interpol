cba6499cead13af8d6f60688342e3a928f47e830
Arron Frasier <Arron.Frasier@ClaytonHomes.com>
2020-10-09 14:53:59 -0400

    Revert "removed unnecessary code file"
    This reverts commit 2e42481e45ce3c9ca21f1a6e2a15ccd94d34651f.

---
+++ CMH.CS.ERP.IntegrationHub.Interpol/Processors/OracleBackflowAPPaymentRequestProcessor.cs
@@ -0,0 +1,56 @@
+﻿using CMH.CS.ERP.IntegrationHub.Interpol.Interfaces.Biz;
+using CMH.CSS.ERP.IntegrationHub.CanonicalModels;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CMH.CS.ERP.IntegrationHub.Interpol.Biz
+{
+    /// <summary>
+    /// APPaymentRequest specfic implementation of the backflow processor
+    /// </summary>
+    public class OracleBackflowAPPaymentRequestProcessor : OracleBackflowProcessor<APPaymentRequest>
+    {
+        /// <summary>
+        /// DI constructor
+        /// </summary>
+        /// <param name="apInvoiceProcessor">The APInvoice backflow processor</param>
+        /// <param name="logger">The class logger</param>
+        /// <param name="rootElement">The root element to search for when parsing backflow XML</param>
+        public OracleBackflowAPPaymentRequestProcessor(
+            ILogger<OracleBackflowAPPaymentRequestProcessor> logger,
+            string rootElement
+        ) : base(logger, rootElement)
+        {
+        }
+
+        /// <summary>
+        /// Processes all AP Payment Request items from the xml string
+        /// </summary>
+        /// <param name="xmlString">XML string from Oracle</param>
+        /// <param name="businessUnit"></param>
+        /// <returns></returns>
+        public override IProcessingResultSet<APPaymentRequest> ProcessItems(string xmlString, string businessUnit)
+        {
+            var parsedItems = _apInvoiceProcessor.ProcessItems(xmlString, businessUnit);
+           
+            // TODO: PAYEEs are not presently returned from Oracle. When they are added, code will need to be added here to handle them as well.
+            List<IProcessingResult<APPaymentRequest>> processingResults = new List<IProcessingResult<APPaymentRequest>>();
+ 
+            return new ProcessingResultSet<APPaymentRequest>()
+            {
+                ProcessedItems = parsedItems.ProcessedItems.Select((a) =>
+                    new ProcessingResult<APPaymentRequest>() 
+                    {
+                        ProcessedItem = new APPaymentRequest() 
+                        {  
+                            Invoice = a.ProcessedItem 
+                        }
+
+                    }).ToList(),
+
+                UnparsableItems = parsedItems.UnparsableItems
+            };
+        }
+    }
+}
